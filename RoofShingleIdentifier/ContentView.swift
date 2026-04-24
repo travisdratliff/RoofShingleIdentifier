15814f7ee505b0fd997a78685130d7ea0b290fda
Travis Ratliff <travis.ratliff14@gmail.com>
2026-04-23 22:50:16 -0500

    Initial Commit

---
+++ RoofShingleIdentifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoofShingleIdentifier
+//
+//  Created by Travis Domenic Ratliff on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoofShingleIdentifier/RoofShingleIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoofShingleIdentifierApp.swift
+//  RoofShingleIdentifier
+//
+//  Created by Travis Domenic Ratliff on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct RoofShingleIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
